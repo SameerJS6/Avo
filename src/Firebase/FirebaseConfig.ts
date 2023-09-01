1f2847640b40a8434919d05da1a43c5785186a0f
Sameer Singh <sameersingh1234pok@gmail.com>
2023-09-01 19:55:03 +0530

    Update Firebase configuration to use environment variables

--- src/Firebase/FirebaseConfig.ts
+++ src/Firebase/FirebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 // import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1EXFdbKfcrLdV_52XSk5491uMImMZaoY",
-  authDomain: "test-auth-22b11.firebaseapp.com",
-  projectId: "test-auth-22b11",
-  storageBucket: "test-auth-22b11.appspot.com",
-  messagingSenderId: "225100695094",
-  appId: "1:225100695094:web:ec73a894a7ac07935d3a97",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
