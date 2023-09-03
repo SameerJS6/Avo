8dba75947873f9ba8ed5ab2123851193a9e00d72
Sameer Singh <sameersingh1234pok@gmail.com>
2023-09-03 20:45:17 +0530

    Update toast styling

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -7,13 +7,13 @@ export default function Toast({}: Props) {
     showAlert: { type, message },
   } = useAuth();
   return (
-    <div className="toast  toast-top toast-end">
+    <div className="toast z-10 toast-top toast-end">
       <div
         className={`${
           type === "error"
             ? "bg-red-600 text-error-content"
             : "bg-success text-success-content"
-        } alert alert-info capitalize`}
+        } alert alert-info capitalize z-10`}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
