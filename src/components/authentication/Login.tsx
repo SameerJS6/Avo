d06a958566670b0091bba3992e1005a4585db1c8
Sameer Singh <sameersingh1234pok@gmail.com>
2023-09-03 20:43:52 +0530

    Update login component to use context and add form validation

--- src/components/authentication/Login.tsx
+++ src/components/authentication/Login.tsx
@@ -1,10 +1,33 @@
-import { useRef } from "react";
-import { Link } from "react-router-dom";
+import { useState, FormEvent } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 type Props = {};
 
 export default function Login({}: Props) {
-  let usernameRef = useRef(null);
-  let passwordRef = useRef(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const { setShowAlert, logIn } = useAuth();
+
+  const handleLogIn = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await logIn(username, password);
+      setShowAlert({
+        show: true,
+        type: "success",
+        message: "Account Logged In Successfully",
+      });
+      navigate("/account");
+      console.log("Logged In");
+    } catch (error) {
+      let message;
+      if (error instanceof Error) message = error.message;
+      else message = String(error);
+      console.error(message);
+    }
+  };
   return (
     <main className="grid place-content-center min-h-screen">
       <div className="card w-[500px] shadow-md hover:shadow-lg">
@@ -14,14 +37,17 @@ export default function Login({}: Props) {
             <p className="text-sm">Welcome back! Please enter your details</p>
           </div>
 
-          <form className="form-control w-full max-w-lg">
+          <form onSubmit={handleLogIn} className="form-control w-full max-w-lg">
             <label htmlFor="username" className="label">
               <span className="label-text">Username or E-mail</span>
             </label>
             <input
               type="email"
-              ref={usernameRef}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
               id="username"
+              required
+              autoFocus
               placeholder="Enter Your Username"
               className="input input-bordered w-full max-w-lg"
             />
@@ -31,18 +57,20 @@ export default function Login({}: Props) {
             </label>
             <input
               type="password"
-              ref={passwordRef}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               id="password"
+              required
               placeholder="Enter Your Password"
               className="input input-bordered w-full max-w-lg"
             />
+
+            <button className="mt-8 btn btn-block tracking-wider btn-primary">
+              Log In
+            </button>
           </form>
 
-          <div className="card-actions mt-4">
-            <button className="btn btn-block tracking-wider btn-primary">
-              Log I n
-            </button>
-          </div>
+          {/* <div className="card-actions mt-4"></div> */}
           <div className="text-center">
             <p className="text-base-content text-opacity-60">
               Don't have an account?{" "}
