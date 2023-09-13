9ec4a02cd2d65963ce80064073f9c7d0bf60b40a
Sameer Singh <sameersingh1234pok@gmail.com>
2023-09-13 22:20:47 +0530

    Shadcn Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import { Route, Routes } from "react-router-dom";
+import { Button } from "./components/ui/button";
+import { Toaster } from "./components/ui/toaster";
+import { toast } from "./components/ui/use-toast";
 
 type Props = {};
 
 export default function App({}: Props) {
-  return <main className="relative "></main>;
+  return (
+    <main className="grid min-h-screen place-content-center">
+      {/* Hello World */}
+      <Button
+        onClick={() => {
+          toast({
+            title: "Scheduled: Catch up",
+            description: "Friday, February 10, 2023 at 5:57 PM",
+          });
+        }}
+      >
+        Hello World
+      </Button>
+      <Toaster />
+    </main>
+  );
 }
