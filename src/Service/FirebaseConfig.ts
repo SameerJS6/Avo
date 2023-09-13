8a893c00f0d1bf320e6bdf3f0b04dc18cac8d13b
Sameer Singh <sameersingh1234pok@gmail.com>
2023-09-13 22:20:35 +0530

    Refactored

---
+++ src/Service/FirebaseConfig.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const APIKEY = import.meta.env.VITE_FIREBASE_API_KEY;
+const AUTHDOMAIN = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
+const PROJECTID = import.meta.env.VITE_FIREBASE_PROJECT_ID;
+const STORAGEBUCKET = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
+const SENDERID = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
+const APPID = import.meta.env.VITE_FIREBASE_APP_ID;
+
+const firebaseConfig = {
+  apiKey: APIKEY,
+  authDomain: AUTHDOMAIN,
+  projectId: PROJECTID,
+  storageBucket: STORAGEBUCKET,
+  messagingSenderId: SENDERID,
+  appId: APPID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
