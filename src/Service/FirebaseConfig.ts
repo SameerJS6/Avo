9a60c849f46244ce8cbeafeeadbde771f5d062b2
Sameer Singh <sameersingh1234pok@gmail.com>
2023-09-13 23:58:22 +0530

    Google Provider Added

--- src/Service/FirebaseConfig.ts
+++ src/Service/FirebaseConfig.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
 
 const APIKEY = import.meta.env.VITE_FIREBASE_API_KEY;
 const AUTHDOMAIN = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
@@ -19,5 +19,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider()
 
-export { app, auth };
+export { app, auth, googleProvider };
