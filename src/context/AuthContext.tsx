53ccf0b781e618ab7969543f63d24ebe22857e37
Sameer Singh <sameersingh1234pok@gmail.com>
2023-09-13 23:56:47 +0530

    Auth Context Added

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,73 @@
+import { auth, googleProvider } from "@/Service/FirebaseConfig";
+import {
+  User,
+  UserCredential,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import {
+  createContext,
+  useState,
+  useContext,
+  ReactNode,
+  useEffect,
+} from "react";
+
+interface AuthContextTypes {
+  currentUser: User | null;
+  logout: () => Promise<void>;
+  loginWithGoogle: () => Promise<UserCredential>;
+  signUp: (email: string, password: string) => Promise<UserCredential>;
+  login: (email: string, password: string) => Promise<UserCredential>;
+}
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+const AuthContext = createContext({} as AuthContextTypes);
+
+const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [currentUser, setCurrentUser] = useState<null | User>(null);
+  const [isWaiting, setIsWaiting] = useState(true);
+
+  const signUp = (email: string, password: string) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const login = (email: string, password: string) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const loginWithGoogle = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const logout = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setIsWaiting(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ currentUser, logout, signUp, login, loginWithGoogle }}
+    >
+      {!isWaiting && children}
+    </AuthContext.Provider>
+  );
+};
+
+const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export { useAuth, AuthProvider };
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
