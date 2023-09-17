cb12da8377f1ef7f9f8dfb85b5229fe6da079823
Sameer Singh <sameersingh1234pok@gmail.com>
2023-09-17 18:13:32 +0530

    Minor Styling Change

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,22 +4,30 @@ import { Button } from "@/components/ui/button";
 import { Pencil1Icon, TrashIcon } from "@radix-ui/react-icons";
 import EditTodo from "./EditTodo";
 import { Tooltip } from "@nextui-org/react";
-// import { Spinner } from "@nextui-org/react"
+import { Spinner } from "@nextui-org/react";
 
 type Props = {};
 
 export default function Home({}: Props) {
-  const { todos, removeTodo, handleEditDialog } = useNotes();
-  // const [isEditing, setIsEditing] = useState(false);
+  const { todos, removeTodo, handleEditDialog, isTodoFetching } = useNotes();
+
+  if (isTodoFetching) return <Spinner size="lg" className="centered" />;
+
+  if (todos.length === 0)
+    return <h1 className="centered">Nothing to see here!</h1>;
+
   return (
-    <>
-      <main className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3 py-8 px-4 sm:py-16 sm:px-8 lg:p-24 xl:p-28">
+    <main className="py-8 px-4 sm:py-16 sm:px-8 lg:p-24 xl:p-28 xl:py-20 space-y-10 md:space-y-16">
+      <h1 className="text-4xl  uppercase tracking-tight sm:text-5xl lg:text-6xl">
+        Todos
+      </h1>
+      <div className="min-[550px]:columns-2 lg:columns-3 gap-4 ">
         {todos.map((todo) => {
           const { description, title, id } = todo;
           return (
             <article
               key={title}
-              className="bg-secondary text-secondary-foreground p-4 rounded-xl space-y-4 hover:shadow-md group"
+              className="bg-secondary break-inside-avoid mb-4 sm:mb-4 text-secondary-foreground p-4 rounded-xl space-y-4 hover:shadow-md group border border-primary-50 hover:border-primary-100"
             >
               <div className="flex items-center justify-between">
                 <h1 className="text-xl font-semibold tracking-tight">
@@ -66,10 +74,10 @@ export default function Home({}: Props) {
             </article>
           );
         })}
-      </main>
+      </div>
       <CreateTodo />
 
       <EditTodo />
-    </>
+    </main>
   );
 }
