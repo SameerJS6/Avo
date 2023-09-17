b12063103bb94f932697ac1b248daaaee87da631
Sameer Singh <sameersingh1234pok@gmail.com>
2023-09-17 23:22:22 +0530

    NavLinks Added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,12 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
+  Tooltip,
 } from "@nextui-org/react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { buttonVariants } from "./ui/button";
 import Logout from "./Auth/Logout";
+import { SunIcon } from "@radix-ui/react-icons";
 
 type Props = {};
 
@@ -14,51 +16,49 @@ export default function Navbar({}: Props) {
   return (
     <NavbarUI shouldHideOnScroll>
       <NavbarBrand>
-        <p className="font-bold text-inherit">ACME</p>
+        <Link to="/" className="font-bold text-3xl text-inherit">
+          ACME
+        </Link>
       </NavbarBrand>
 
       <NavbarContent justify="end" className="items-center">
-        <NavbarItem isActive>
-          <Link to="/home" className={buttonVariants({ variant: "outline" })}>
-            <div className="flex gap-1.5 items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-5 h-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                />
-              </svg>
-              Home
-            </div>
-          </Link>
-        </NavbarItem>
-        <NavbarItem className="hidden sm:flex">
-          <Link to="/trash" className={buttonVariants({ variant: "outline" })}>
-            <div className="flex gap-1 5 items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-5 h-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                />
-              </svg>
-              Trash
-            </div>
-          </Link>
+        <Tooltip
+          content="View Todos"
+          closeDelay={50}
+          color="foreground"
+          classNames={{
+            base: "py-2 px-4 shadow-xl rounded-lg",
+          }}
+        >
+          <NavbarItem isActive>
+            <NavLink
+              to="/dashboard"
+              className={buttonVariants({ variant: "ghost", size: "icon" })}
+              
+            >
+              <div className="flex gap-1.5 items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z"
+                  />
+                </svg>
+              </div>
+            </NavLink>
+          </NavbarItem>
+        </Tooltip>
+        <NavbarItem
+          className={buttonVariants({ variant: "ghost", size: "icon" })}
+        >
+          <SunIcon />
         </NavbarItem>
         <NavbarItem className="flex">
           <Logout />
