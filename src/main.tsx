adb36ccee89f93328ad259f53777430bd2d36db4
Sameer Singh <sameersingh1234pok@gmail.com>
2023-09-22 22:07:09 +0530

    Theme Context Added

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,20 @@ import { NextUIProvider } from "@nextui-org/react";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import { NotesProvider } from "./context/NotesContext.tsx";
+import { ThemeProvider } from "./context/ThemeContent.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <NextUIProvider>
-        <AuthProvider>
-          <NotesProvider>
-            <App />
-          </NotesProvider>
-        </AuthProvider>
-      </NextUIProvider>
+      <ThemeProvider>
+        <NextUIProvider>
+          <AuthProvider>
+            <NotesProvider>
+              <App />
+            </NotesProvider>
+          </AuthProvider>
+        </NextUIProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
