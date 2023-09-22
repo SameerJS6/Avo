dd2e50ec9aedcb08cdac4a237f8e78d8bfe94a5d
Sameer Singh <sameersingh1234pok@gmail.com>
2023-09-22 22:11:02 +0530

    Todo Animation Added

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -2,12 +2,15 @@ import { useNotes } from "@/context/NotesContext";
 import { Checkbox } from "@nextui-org/react";
 import EditButton from "./EditButton";
 import DeleteButton from "./DeleteButton";
+import { motion } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 type TodoProps = {
   id: string;
   title: string;
   description: string;
   isCompleted: boolean;
+  index: number;
 };
 
 export default function Todo({
@@ -15,27 +18,49 @@ export default function Todo({
   title,
   id,
   isCompleted,
+  index,
 }: TodoProps) {
   const { updateComplete } = useNotes();
+  let hasRenderedReminderRef = useRef(false);
+  useEffect(() => {
+    hasRenderedReminderRef.current = true;
+  }, []);
 
   return (
-    <article
+    <motion.article
       key={title}
-      data-overlay = {isCompleted}
+      data-overlay={isCompleted}
       className={`${
         isCompleted
           ? "bg-gradient-to-tr from-muted/80 to-border/25 text-muted-foreground line-through scale-[97%]"
-          : "group bg-gradient-to-tl from-secondary text-secondary-foreground hover:shadow-md  scale-100 hover:border-primary/20"
-      } relative break-inside-avoid mb-4 sm:mb-4  p-4 rounded-xl space-y-2 border border-border md:space-y-4 transition-all duration-250 ease-in-out`}
+          : "group bg-gradient-to-tl from-secondary text-secondary-foreground hover:shadow-md scale-100 hover:border-primary/20"
+      } relative p-4 grid rounded-xl space-y-2 border border-border md:space-y-4 transition-all duration-250 ease-in-out`}
+      variants={{
+        hidden: (index) => ({
+          opacity: 0,
+          y: 10 * index,
+        }),
+        visible: (index) => ({
+          opacity: 1,
+          y: 0,
+          transition: { delay: index * 0.05 },
+        }),
+        removed: {
+          opacity: 0,
+        },
+      }}
+      initial={hasRenderedReminderRef.current ? "visible" : "hidden"}
+      animate="visible"
+      exit="removed"
+      custom={index}
     >
       <h1 className="text-lg sm:text-xl font-semibold tracking-tight">
         {title}
       </h1>
 
-      <div className="">
-        <p className="leading-6">{description}</p>
-      </div>
-      <div className="flex items-center flex-row-reverse justify-between gap-4">
+      <p className="leading-6">{description}</p>
+
+      <div className="flex items-center self-end flex-row-reverse justify-between gap-4">
         <div className="space-x-4">
           <EditButton id={id} />
           <DeleteButton id={id} />
@@ -55,6 +80,6 @@ export default function Todo({
           />
         </form>
       </div>
-    </article>
+    </motion.article>
   );
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,82 +1,41 @@
 import { useNotes } from "@/context/NotesContext";
 import CreateTodo from "./CreateTodo";
-import { Button } from "@/components/ui/button";
-import { Pencil1Icon, TrashIcon } from "@radix-ui/react-icons";
 import EditTodo from "./EditTodo";
-import { Tooltip } from "@nextui-org/react";
 import { Spinner } from "@nextui-org/react";
 import Todo from "@/components/Todo";
+import { AnimatePresence, motion } from "framer-motion";
 
 type Props = {};
 
 export default function Dashboard({}: Props) {
-  const { todos, removeTodo, handleEditDialog, isWaiting } = useNotes();
+  const { todos, isWaiting } = useNotes();
 
   if (isWaiting.isTodoFetching)
     return <Spinner size="lg" className="centered" />;
 
   if (todos.length === 0)
-    return <h1 className="centered">Nothing to see here!</h1>;
+    return (
+      <motion.h1
+        className="centered"
+        initial={{ opacity: 0, y: 10, scale: 0.98 }}
+        animate={{ opacity: 1, y: 0, scale: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        Nothing to see here!
+      </motion.h1>
+    );
 
   return (
     <main className="py-8 px-4 sm:py-16 sm:px-8 lg:p-24 xl:p-28 xl:py-20 space-y-10 md:space-y-16">
-      <h1 className="text-4xl  uppercase tracking-tight sm:text-5xl lg:text-6xl">
+      <h1 className="text-4xl uppercase tracking-tight sm:text-5xl lg:text-6xl">
         Todos
       </h1>
-      <div className="min-[550px]:columns-2 lg:columns-3 gap-4 ">
-        {todos.map((todo) => {
-          //   const { description, title, id }
-          return (
-            <Todo key={todo.id} {...todo} />
-            // <article
-            //   key={title}
-            //   className="bg-secondary break-inside-avoid mb-4 sm:mb-4 text-secondary-foreground p-4 rounded-xl space-y-4 hover:shadow-md group border border-primary-100 hover:border-primary-200"
-            // >
-            //   <div className="flex items-center justify-between">
-            //     <h1 className="text-xl font-semibold tracking-tight">
-            //       {title}
-            //     </h1>
-            //     <Tooltip
-            //       content="Delete Note"
-            //       closeDelay={50}
-            //       color="foreground"
-            //       classNames={{
-            //         base: "py-2 px-4 shadow-xl rounded-lg",
-            //       }}
-            //     >
-            //       <Button
-            //         onClick={() => removeTodo(id)}
-            //         variant="destructive"
-            //         size="icon"
-            //         className="pointer-events-none group-hover:pointer-events-auto opacity-0 group-hover:opacity-100 rounded-xl transition-all duration-250 ease-in-out"
-            //       >
-            //         <TrashIcon />
-            //       </Button>
-            //     </Tooltip>
-            //   </div>
-            //   <div className="space-y-2">
-            //     <p className="leading-6">{description}</p>
-            //     <Tooltip
-            //       content="Edit Note"
-            //       closeDelay={50}
-            //       color="foreground"
-            //       classNames={{
-            //         base: "py-2 px-4 shadow-xl rounded-lg",
-            //       }}
-            //     >
-            //       <Button
-            //         onClick={() => handleEditDialog(id)}
-            //         size="icon"
-            //         variant="outline"
-            //         className="pointer-events-none opacity-0 group-hover:opacity-100 group-hover:pointer-events-auto tranition-all duration-250 ease-soft-spring"
-            //       >
-            //         <Pencil1Icon />
-            //       </Button>
-            //     </Tooltip>
-            //   </div>
-            // </article>
-          );
-        })}
+      <div className="grid min-[550px]:grid-cols-2 lg:grid-cols-3 gap-4">
+        <AnimatePresence>
+          {todos.map((todo, index) => {
+            return <Todo key={todo.id} {...todo} index={index} />;
+          })}
+        </AnimatePresence>
       </div>
       <CreateTodo />
       <EditTodo />
