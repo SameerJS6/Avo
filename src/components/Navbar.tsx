331e89521e2cddee9c88a184dddba4fa186e3232
Sameer Singh <sameersingh1234pok@gmail.com>
2023-09-23 20:53:09 +0530

    Update to support user authentication.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,10 +9,12 @@ import Logout from "./Auth/Logout";
 import Sidebar from "./Sidebar";
 import ThemeToggle from "./ThemeToggle";
 import { Separator } from "./ui/separator";
+import { useAuth } from "@/context/AuthContext";
 
 type Props = {};
 
 export default function Navbar({}: Props) {
+  const { currentUser } = useAuth();
   return (
     <NavbarUI shouldHideOnScroll>
       <NavbarBrand>
@@ -20,48 +22,56 @@ export default function Navbar({}: Props) {
           <Link to="/" className="font-bold text-3xl text-inherit">
             AVO
           </Link>
-          <div className="grid lg:hidden items-center">
-            <Sidebar />
-          </div>
+          {currentUser && (
+            <div className="grid lg:hidden items-center">
+              <Sidebar />
+            </div>
+          )}
         </div>
       </NavbarBrand>
 
-      <NavbarContent justify="end" className="items-center hidden lg:flex ">
-        {/* <NavbarItem> */}
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive
-              ? "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 h-9 px-4 py-2 rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-              : "hover:bg-accent hover:text-accent-foreground rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 h-9 px-4 py-2"
-          }
-        >
-          Home
-        </NavLink>
-        {/* </NavbarItem> */}
-        {/* <NavbarItem> */}
-        <NavLink
-          to="/dashboard"
-          className={({ isActive }) =>
-            isActive
-              ? "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 h-9 px-4 py-2 rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
-              : "hover:bg-accent hover:text-accent-foreground rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 h-9 px-4 py-2"
-          }
-        >
-          {/* <Button variant="ghost">Todos</Button> */}
-          Todos
-        </NavLink>
-        {/* </NavbarItem> */}
-        <Separator orientation="vertical" className="h-8" />
-        <div className="flex gap-2">
+      {currentUser && (
+        <NavbarContent justify="end" className="items-center hidden lg:flex ">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 h-9 px-4 py-2 rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+                : "hover:bg-accent hover:text-accent-foreground rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 h-9 px-4 py-2"
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/dashboard"
+            className={({ isActive }) =>
+              isActive
+                ? "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 h-9 px-4 py-2 rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+                : "hover:bg-accent hover:text-accent-foreground rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 h-9 px-4 py-2"
+            }
+          >
+            Todos
+          </NavLink>
+          <Separator orientation="vertical" className="h-8" />
+
+          <div className="flex gap-2">
+            <NavbarItem>
+              <ThemeToggle />
+            </NavbarItem>
+            <NavbarItem>
+              <Logout style />
+            </NavbarItem>
+          </div>
+        </NavbarContent>
+      )}
+
+      {!currentUser && (
+        <NavbarContent justify="end">
           <NavbarItem>
-            <ThemeToggle />
-          </NavbarItem>
-          <NavbarItem className="flex">
             <Logout style />
           </NavbarItem>
-        </div>
-      </NavbarContent>
+        </NavbarContent>
+      )}
     </NavbarUI>
   );
 }
